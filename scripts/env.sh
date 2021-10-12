94eee6c6792eb38fa984fca23efd90540f6d58ed
Samuel Li <samli@samuelli.net>
2021-10-12 18:52:29 +1100

    modified workflow to run on all pushes

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,4 @@
-dockerhub_repo="samuellihy"
+dockerhub_repo="YOUR_DOCKERHUB_USERNAME"
 
 function get_sanitised_branch_name() {
   local branch_name=$(git rev-parse --abbrev-ref HEAD | sed -E 's/[^0-9A-Za-z\-_]/-/g')
